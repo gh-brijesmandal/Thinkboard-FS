25c351b24ba4cdb9561446ff6921d72a567c518b
Brijes Mandal <brijes.mandal01@gmail.com>
2025-11-30 15:56:53 -0600

    API setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get()
+app.get("/api/notes", (req,res) => {
+    res.status(200).send("You got 20 notes");
+}).post("/api/notes", (req,res) => {
+    res.status(201).json({
+        message: "Note created successfully!"
+    })
+})
+
+app.put("/api/notes/:id", (req,res) => {
+    res.status(200).json({
+        message: "Note updated successfully!"
+    })
+}).delete((req,res) => {
+    res.status(200).json({ message: "Note deleted successfully!" })
+})
+
+
 
 
 app.listen(8000, () => {
